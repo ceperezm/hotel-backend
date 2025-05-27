caa209b7ab2a5ebe089d9e37cc35e78168ee67e3
Jeferson Florez <jeffersonflorez73@gmail.com>
2025-05-27 11:38:48 -0500

    poniendo @EnableMethodSecurity

--- src/main/java/com/hotel/backend/security/config/SecurityConfig.java
+++ src/main/java/com/hotel/backend/security/config/SecurityConfig.java
@@ -3,13 +3,12 @@ package com.hotel.backend.security.config;
 import com.hotel.backend.security.jwt.JwtFilter;
 import com.hotel.backend.security.service.JpaUserDetailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,7 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
